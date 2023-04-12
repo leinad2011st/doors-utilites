cbcc2fb099f6d67187f01b8d21d70be2b27081aa
leinad2011st <91507903+leinad2011st@users.noreply.github.com>
2023-04-12 17:59:44 -0300

    Update main.lua

--- UNNAMEDDOORS/main.lua
+++ UNNAMEDDOORS/main.lua
@@ -98,6 +98,62 @@ serverMain:AddToggle('OpenDoor50NoCutscene', {
     end
 })
 
+_G.Keybind = "N"
+_G.IncludeNoclip = true
+
+local isEnabled = false
+
+local UIS = game:GetService("UserInputService")
+
+local Plr = game.Players.LocalPlayer
+local Char = Plr.Character or Plr.CharacterAdded:Wait()
+
+function getValue()
+    local value
+    if isEnabled then
+        value = "On"
+    else
+        value = "Off"
+    end
+    return value
+end
+
+function ToggleNoclip()
+    isEnabled = not isEnabled
+end
+
+UIS.InputBegan:Connect(function(input, gp)
+    if gp then return end
+
+    if input.KeyCode == Enum.KeyCode[_G.Keybind] then
+        ToggleNoclip()
+        task.wait()
+    end
+end)
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    if not Char:FindFirstChild("HumanoidRootPart") then return end
+    if _G.IncludeNoclip then
+        Char.HumanoidRootPart.CanCollide = not isEnabled
+        Char.Collision.CanCollide = not isEnabled
+    end
+
+    local HrpCFrame = Char.HumanoidRootPart.CFrame
+
+    local ray = Ray.new(HrpCFrame.Position, HrpCFrame.LookVector * 0.5)
+    local part = workspace:FindPartOnRay(ray)
+    if part and part.CanCollide == true and isEnabled then
+        Char.HumanoidRootPart.Anchored = true
+        Char:PivotTo(Char.HumanoidRootPart.CFrame * CFrame.new(0, 1000, 0))
+        task.wait()
+        Char:PivotTo(Char.HumanoidRootPart.CFrame * CFrame.new(0, 0, -4))
+        task.wait()
+        Char:PivotTo(Char.HumanoidRootPart.CFrame * CFrame.new(0, -1000, 0))
+        task.wait(0.1)
+        Char.HumanoidRootPart.Anchored = false
+    end
+end)
+
 
 client:AddToggle('Toggle WalkSpeed', {
     Text = 'Toggle Walk Speed',
@@ -169,6 +225,15 @@ client:AddToggle('InstantInteract',{
     Tooltip = 'Disables HoldDaration for ProximityPrompts',
     Callback = function(Value)
         flags.InstantInteract=Value
+    end
+})
+
+client:AddToggle('NoClip',{
+    Text = 'NOCLIP (N)',
+    default = false,
+    Tooltip = 'noclips with a keybind',
+    Callback = function(Value)
+        ToggleNoclip()
     end
 })
 

7934c1d3de903c15ddd0bc369b0d225a72f4f041
leinad2011st <91507903+leinad2011st@users.noreply.github.com>
2023-04-14 19:34:53 -0300

    Update main.lua

--- UNNAMEDDOORS/main.lua
+++ UNNAMEDDOORS/main.lua
@@ -112,7 +112,7 @@ serverMain:AddToggle('OpenDoor50NoCutscene', {
         end
     end
 })
-serverMain:AddToggle('AgressiveFigure',{
+serverMain:AddButton({
     Text = "Agressive Figure",
     Tooltip = 'only works after opening door 50',
     DoubleClick = false,

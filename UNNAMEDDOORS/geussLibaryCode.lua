b8b234b13b6cef4a1b2d3d67507edeb121f8567f
leinad2011st <91507903+leinad2011st@users.noreply.github.com>
2023-04-25 16:32:06 -0300

    Update geussLibaryCode.lua

--- UNNAMEDDOORS/geussLibaryCode.lua
+++ UNNAMEDDOORS/geussLibaryCode.lua
@@ -1,16 +1,18 @@
+
 local LibaryCodeGeusser = {}
 
-function LibaryCodeGeusser () 
+
+
+
+function LibaryCodeGeusser:Geuss(LibaryCodeLength) 
     for x = 0,99999 do 
-        if tostring(x):len() == 5 then 
             wait()
-            game:GetService("ReplicatedStorage").EntityInfo.PL:FireServer(tostring(x))
-        else 
-        
-        end
+            local howmuch = LibaryCodeLength - tostring(x):len()
+            local geuss = tostring(x)..tostring(math.pow(10,3+1)):sub(2,howmuch+1)
+            print(geuss)
+            game:GetService("ReplicatedStorage").EntityInfo.PL:FireServer(tostring(geuss))
     end
    
 end
-
 
 getgenv().LibaryCodeGeusser = LibaryCodeGeusser

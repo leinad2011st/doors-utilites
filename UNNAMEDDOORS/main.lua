c024cfb794a2271a244a8f6343f8311af78fc147
leinad2011st <91507903+leinad2011st@users.noreply.github.com>
2023-08-26 13:09:30 -0300

    Update main.lua

--- UNNAMEDDOORS/main.lua
+++ UNNAMEDDOORS/main.lua
@@ -572,21 +572,6 @@ game:GetService("RunService").RenderStepped:Connect(function()
         Char.HumanoidRootPart.CanCollide = not isEnabled
         Char.Collision.CanCollide = not isEnabled
     -- end
-
-    local HrpCFrame = Char.HumanoidRootPart.CFrame
-
-    local ray = Ray.new(HrpCFrame.Position, HrpCFrame.LookVector * 0.5)
-    local part = workspace:FindPartOnRay(ray)
-    if part and part.CanCollide == true and isEnabled then
-        Char.HumanoidRootPart.Anchored = true
-        Char:PivotTo(Char.HumanoidRootPart.CFrame * CFrame.new(0, 1000, 0))
-        task.wait()
-        Char:PivotTo(Char.HumanoidRootPart.CFrame * CFrame.new(0, 0, -4))
-        task.wait()
-        Char:PivotTo(Char.HumanoidRootPart.CFrame * CFrame.new(0, -1000, 0))
-        task.wait(0.1)
-        Char.HumanoidRootPart.Anchored = false
-    end
 end)
 
 
